506bdc700c7c109292a19e9b0c4173568be23bae
Paul Von Zimmerman <vonzimp@gmail.com>
2021-02-25 15:55:24 -0800

    Add docx

--- compile_pdf.sh
+++ compile_pdf.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 pandoc -o resume.html -c resume-css-stylesheet.css -s resume.md;
+pandoc -o resume.docx -c resume-css-stylesheet.css -s resume.md;
 ./wkhtmltox/bin/wkhtmltopdf --enable-local-file-access resume.html resume.pdf
